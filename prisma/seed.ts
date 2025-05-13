51f9427a57dd00a33c83cc305bfe70ce6478fc05
Yassine Gherbi <y.gherbi.dev@gmail.com>
2025-05-13 03:19:31 +0200

    feat: Add super admin user to seed data and standardize hashed passwords for existing users

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -18,32 +18,38 @@ const performanceScales = [
 // Sample users for testing
 const users = [
   {
+    email: 'superadmin@emuready.com',
+    hashedPassword: bcryptjs.hashSync('password', 10),
+    name: 'Super Admin User',
+    role: Role.SUPER_ADMIN,
+  },
+  {
     email: 'admin@emuready.com',
-    hashedPassword: bcryptjs.hashSync('password123', 10),
+    hashedPassword: bcryptjs.hashSync('password', 10),
     name: 'Admin User',
     role: Role.ADMIN,
   },
   {
     email: 'author@emuready.com',
-    hashedPassword: bcryptjs.hashSync('password123', 10),
+    hashedPassword: bcryptjs.hashSync('password', 10),
     name: 'Author User',
     role: Role.AUTHOR,
   },
   {
     email: 'user1@emuready.com',
-    hashedPassword: bcryptjs.hashSync('password123', 10),
+    hashedPassword: bcryptjs.hashSync('password', 10),
     name: 'Regular User 1',
     role: Role.USER,
   },
   {
     email: 'user2@emuready.com',
-    hashedPassword: bcryptjs.hashSync('password123', 10),
+    hashedPassword: bcryptjs.hashSync('password', 10),
     name: 'Regular User 2',
     role: Role.USER,
   },
   {
     email: 'user3@emuready.com',
-    hashedPassword: bcryptjs.hashSync('password123', 10),
+    hashedPassword: bcryptjs.hashSync('password', 10),
     name: 'Regular User 3',
     role: Role.USER,
   },
