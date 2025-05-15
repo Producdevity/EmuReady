ab5a05ebe3ae7d57ce7a16700e12e9fddf6b6bd3
Yassine Gherbi <y.gherbi.dev@gmail.com>
2025-05-15 16:51:18 +0200

    refactor: Remove unnecessary description field in performanceScalesSeeder

--- prisma/seeders/performanceScalesSeeder.ts
+++ prisma/seeders/performanceScalesSeeder.ts
@@ -1,17 +1,20 @@
 import { type PrismaClient, type PerformanceScale } from '@orm'
 
-type PerformanceScaleData = Pick<
-  PerformanceScale,
-  'label' | 'rank' | 'description'
->
+type PerformanceScaleData = Pick<PerformanceScale, 'label' | 'rank'>
 
 const performanceScales: PerformanceScaleData[] = [
-  { label: 'Nothing', rank: 1, description: 'Does not work at all.' },
-  { label: 'Loadable', rank: 2, description: 'Loads but does not play.' },
-  { label: 'Intro', rank: 3, description: 'Does not play past intro or menu.' },
-  { label: 'Ingame', rank: 4, description: 'Plays but has major issues.' },
-  { label: 'Playable', rank: 5, description: 'Plays but has minor issues.' },
-  { label: 'Perfect', rank: 6, description: 'Plays perfectly.' },
+  { label: 'Nothing', rank: 1 },
+  { label: 'Loadable', rank: 2 },
+  { label: 'Intro', rank: 3 },
+  { label: 'Ingame', rank: 4 },
+  { label: 'Playable', rank: 5 },
+  { label: 'Perfect', rank: 6 },
+  // { label: 'Nothing', rank: 1, description: 'Does not work at all.' },
+  // { label: 'Loadable', rank: 2, description: 'Loads but does not play.' },
+  // { label: 'Intro', rank: 3, description: 'Does not play past intro or menu.' },
+  // { label: 'Ingame', rank: 4, description: 'Plays but has major issues.' },
+  // { label: 'Playable', rank: 5, description: 'Plays but has minor issues.' },
+  // { label: 'Perfect', rank: 6, description: 'Plays perfectly.' },
 ]
 
 async function performanceScalesSeeder(prisma: PrismaClient) {
