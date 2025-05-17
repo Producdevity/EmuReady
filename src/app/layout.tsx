dc7b9aa9656cd872b8969be28c1cd321bd2b1a71
Yassine Gherbi <y.gherbi.dev@gmail.com>
2025-05-17 22:07:43 +0200

    feat: implement error boundary

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,13 @@
 import './globals.css'
-import { type PropsWithChildren } from 'react'
+import { ErrorFallback } from '@/components/ui'
+import { type PropsWithChildren, type ErrorInfo } from 'react'
 import { GoogleAnalytics } from '@next/third-parties/google'
 import { Inter } from 'next/font/google'
 import { SpeedInsights } from '@vercel/speed-insights/next'
 import Providers from '@/components/Providers'
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
-import { ErrorBoundary } from '@/components/ui'
+import { ErrorBoundary } from 'react-error-boundary'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,18 +18,34 @@ export const metadata = {
 
 const GA_ID = process.env.NEXT_PUBLIC_GA_ID
 
+const handleError = (error: Error, info: ErrorInfo) => {
+  // TODO: Log error to an error tracking service
+  console.error('ErrorBoundary caught an error', error, info)
+}
+
 export default function RootLayout(props: PropsWithChildren) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
         <Providers>
-          <ErrorBoundary>
-            <div className="flex flex-col min-h-screen bg-white text-gray-900 dark:bg-gray-900 dark:text-gray-100 transition-colors">
-              <Navbar />
-              <main className="flex-1 flex flex-col">{props.children}</main>
-              <Footer />
-            </div>
-          </ErrorBoundary>
+          <div className="flex flex-col min-h-screen bg-white text-gray-900 dark:bg-gray-900 dark:text-gray-100 transition-colors">
+            <Navbar />
+            <main className="flex-1 flex flex-col">
+              <ErrorBoundary
+                FallbackComponent={ErrorFallback}
+                onReset={(details) => {
+                  console.error('ErrorBoundary reset', details)
+                  // TODO: Log reset event to an error tracking service
+                  // TODO: Reset any state in your app that caused the error instead of reloading
+                  window.location.reload()
+                }}
+                onError={handleError}
+              >
+                {props.children}
+              </ErrorBoundary>
+            </main>
+            <Footer />
+          </div>
         </Providers>
       </body>
       <SpeedInsights />
