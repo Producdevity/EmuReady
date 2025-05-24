b4d4d37e7081b90816f2270048d234055972eb46
Yassine Gherbi <y.gherbi.dev@gmail.com>
2025-05-24 18:57:37 +0200

    refactor: adminNav and superAdminNav

---
+++ src/app/admin/data.ts
@@ -0,0 +1,41 @@
+export const adminNav = [
+  {
+    href: '/admin/systems',
+    label: 'Systems',
+    description: 'Manage game systems and platforms.',
+  },
+  { href: '/admin/brands', label: 'Brands', description: 'Manage brands.' },
+  {
+    href: '/admin/devices',
+    label: 'Devices',
+    description: 'Manage Android devices.',
+  },
+  {
+    href: '/admin/emulators',
+    label: 'Emulators',
+    description: 'Manage emulators.',
+  },
+  {
+    href: '/admin/performance',
+    label: 'Performance Scales',
+    description: 'Manage performance scales.',
+  },
+  {
+    href: '/admin/approvals',
+    label: 'Listing Approvals',
+    description: 'Manage listing approvals.',
+  },
+]
+
+export const superAdminNav = [
+  {
+    href: '/admin/users',
+    label: 'Users Management',
+    description: 'Manage users, roles, and permissions.',
+  },
+  {
+    href: '/admin/processed-listings',
+    label: 'Processed Listings',
+    description: 'Manage processed listings.',
+  },
+]
