d9224ec0f23f4f99ab51bea8ccadddc13dfa6e20
Yassine Gherbi <y.gherbi.dev@gmail.com>
2025-05-25 15:26:09 +0200

    feat: simplify schema definitions in emulator.ts for improved readability

--- src/schemas/emulator.ts
+++ src/schemas/emulator.ts
@@ -1,27 +1,19 @@
 import { z } from 'zod'
 
 export const GetEmulatorsSchema = z
-  .object({
-    search: z.string().optional(),
-  })
+  .object({ search: z.string().optional() })
   .optional()
 
-export const GetEmulatorByIdSchema = z.object({
-  id: z.string().uuid(),
-})
+export const GetEmulatorByIdSchema = z.object({ id: z.string().uuid() })
 
-export const CreateEmulatorSchema = z.object({
-  name: z.string().min(1),
-})
+export const CreateEmulatorSchema = z.object({ name: z.string().min(1) })
 
 export const UpdateEmulatorSchema = z.object({
   id: z.string().uuid(),
   name: z.string().min(1),
 })
 
-export const DeleteEmulatorSchema = z.object({
-  id: z.string().uuid(),
-})
+export const DeleteEmulatorSchema = z.object({ id: z.string().uuid() })
 
 export const UpdateSupportedSystemsSchema = z.object({
   emulatorId: z.string().uuid(),
