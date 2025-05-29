17791b199fbc41039bc065e573e59183da558a1a
Yassine Gherbi <y.gherbi.dev@gmail.com>
2025-05-29 18:03:22 +0200

    feat: my god this is a lot of work.

---
+++ src/components/icons/systems/NintendoGameCubeIcon.tsx
@@ -0,0 +1,25 @@
+function NintendoGameCubeIcon() {
+  return (
+    <svg
+      role="img"
+      xmlns="http://www.w3.org/2000/svg"
+      x="0"
+      y="0"
+      width="100"
+      height="100"
+      viewBox="0 0 24 24"
+    >
+      <title>Nintendo GameCube</title>
+      <path
+        fill="#564FAE"
+        d="m1.5 18.205 10.021 5.792v-3.821l-6.717-3.882V8.526L1.5 6.616zM22.5 6.619l-10.02 5.792v5.431l4.695-2.715v-1.614l2.019-1.167V16.296l-6.715 3.888V24L22.5 18.206zM16.696 8.862 12 6.146v-.001L7.306 8.863 12 11.579z"
+      />
+      <path
+        fill="#564FAE"
+        d="m17.649 7.087 3.304-1.91L12 0 1.992 5.789l3.301 1.907L12 3.813zM11.52 17.841v-5.433L6.823 9.695v5.432l.002-.001z"
+      />
+    </svg>
+  )
+}
+
+export default NintendoGameCubeIcon
