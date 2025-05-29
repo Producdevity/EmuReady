41a47b29d7187ea6dfbd6234cd099fa785303148
Yassine Gherbi <y.gherbi.dev@gmail.com>
2025-05-29 18:44:37 +0200

    feat: add SVG icons for Microsoft Windows and Nintendo consoles

---
+++ src/components/icons/systems/MicrosoftWindowsIcon.tsx
@@ -0,0 +1,18 @@
+// TODO: implement
+function MicrosoftWindowsIcon() {
+  return (
+    <svg
+      role="img"
+      xmlns="http://www.w3.org/2000/svg"
+      x="0"
+      y="0"
+      width="171.81796"
+      height="43.527214"
+      viewBox="0 0 171.81796 43.527214"
+    >
+      <title>Microsoft Windows</title>
+    </svg>
+  )
+}
+
+export default MicrosoftWindowsIcon
---
+++ src/components/icons/systems/NintendoEntertainmentSystemIcon.tsx
@@ -0,0 +1,18 @@
+// TODO: implement
+function NintendoEntertainmentSystemIcon() {
+  return (
+    <svg
+      role="img"
+      xmlns="http://www.w3.org/2000/svg"
+      x="0"
+      y="0"
+      width="100"
+      height="100"
+      viewBox="0 0 24 24"
+    >
+      <title>Nintendo Entertainment System</title>
+    </svg>
+  )
+}
+
+export default NintendoEntertainmentSystemIcon
