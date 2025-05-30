763f93069252341de6af04141d2083d33da3b0e5
Yassine Gherbi <y.gherbi.dev@gmail.com>
2025-05-30 15:28:45 +0200

    fix: i hate how migrations work in prisma

--- prisma/migrations/0_init/migration.sql
+++ prisma/migrations/0_init/migration.sql
@@ -4,6 +4,9 @@ CREATE TYPE "Role" AS ENUM ('USER', 'AUTHOR', 'ADMIN', 'SUPER_ADMIN');
 -- CreateEnum
 CREATE TYPE "CustomFieldType" AS ENUM ('TEXT', 'TEXTAREA', 'URL', 'BOOLEAN', 'SELECT');
 
+-- CreateEnum
+CREATE TYPE "ListingApprovalStatus" AS ENUM ('PENDING', 'APPROVED', 'REJECTED');
+
 -- CreateTable
 CREATE TABLE "User" (
     "id" TEXT NOT NULL,
@@ -39,6 +42,7 @@ CREATE TABLE "Device" (
 CREATE TABLE "System" (
     "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
+    "key" TEXT,
 
     CONSTRAINT "System_pkey" PRIMARY KEY ("id")
 );
@@ -82,6 +86,7 @@ CREATE TABLE "PerformanceScale" (
     "id" SERIAL NOT NULL,
     "label" TEXT NOT NULL,
     "rank" INTEGER NOT NULL,
+    "description" TEXT,
 
     CONSTRAINT "PerformanceScale_pkey" PRIMARY KEY ("id")
 );
@@ -96,6 +101,10 @@ CREATE TABLE "Listing" (
     "notes" TEXT,
     "authorId" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "status" "ListingApprovalStatus" NOT NULL DEFAULT 'PENDING',
+    "processedAt" TIMESTAMP(3),
+    "processedNotes" TEXT,
+    "processedByUserId" TEXT,
 
     CONSTRAINT "Listing_pkey" PRIMARY KEY ("id")
 );
@@ -138,16 +147,23 @@ CREATE TABLE "CommentVote" (
 );
 
 -- CreateTable
-CREATE TABLE "ListingApproval" (
+CREATE TABLE "ListingCustomFieldValue" (
     "id" TEXT NOT NULL,
     "listingId" TEXT NOT NULL,
-    "approvedById" TEXT NOT NULL,
-    "approvedByRole" TEXT NOT NULL,
-    "approvedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "status" TEXT NOT NULL,
-    "notes" TEXT,
+    "customFieldDefinitionId" TEXT NOT NULL,
+    "value" JSONB NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
-    CONSTRAINT "ListingApproval_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "ListingCustomFieldValue_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_EmulatorToSystem" (
+    "A" TEXT NOT NULL,
+    "B" TEXT NOT NULL,
+
+    CONSTRAINT "_EmulatorToSystem_AB_pkey" PRIMARY KEY ("A","B")
 );
 
 -- CreateIndex
@@ -161,6 +177,9 @@ CREATE UNIQUE INDEX "Device_brandId_modelName_key" ON "Device"("brandId", "model
 
 -- CreateIndex
 CREATE UNIQUE INDEX "System_name_key" ON "System"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "System_key_key" ON "System"("key");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Game_title_systemId_key" ON "Game"("title", "systemId");
@@ -178,6 +197,9 @@ CREATE UNIQUE INDEX "Emulator_name_key" ON "Emulator"("name");
 CREATE UNIQUE INDEX "PerformanceScale_label_key" ON "PerformanceScale"("label");
 
 -- CreateIndex
+CREATE INDEX "Listing_processedByUserId_idx" ON "Listing"("processedByUserId");
+
+-- CreateIndex
 CREATE UNIQUE INDEX "Listing_gameId_deviceId_emulatorId_key" ON "Listing"("gameId", "deviceId", "emulatorId");
 
 -- CreateIndex
@@ -185,6 +207,18 @@ CREATE UNIQUE INDEX "Vote_userId_listingId_key" ON "Vote"("userId", "listingId")
 
 -- CreateIndex
 CREATE UNIQUE INDEX "CommentVote_userId_commentId_key" ON "CommentVote"("userId", "commentId");
+
+-- CreateIndex
+CREATE INDEX "ListingCustomFieldValue_listingId_idx" ON "ListingCustomFieldValue"("listingId");
+
+-- CreateIndex
+CREATE INDEX "ListingCustomFieldValue_customFieldDefinitionId_idx" ON "ListingCustomFieldValue"("customFieldDefinitionId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "ListingCustomFieldValue_listingId_customFieldDefinitionId_key" ON "ListingCustomFieldValue"("listingId", "customFieldDefinitionId");
+
+-- CreateIndex
+CREATE INDEX "_EmulatorToSystem_B_index" ON "_EmulatorToSystem"("B");
 
 -- AddForeignKey
 ALTER TABLE "Device" ADD CONSTRAINT "Device_brandId_fkey" FOREIGN KEY ("brandId") REFERENCES "DeviceBrand"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
@@ -199,6 +233,9 @@ ALTER TABLE "CustomFieldDefinition" ADD CONSTRAINT "CustomFieldDefinition_emulat
 ALTER TABLE "Listing" ADD CONSTRAINT "Listing_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
+ALTER TABLE "Listing" ADD CONSTRAINT "Listing_processedByUserId_fkey" FOREIGN KEY ("processedByUserId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
 ALTER TABLE "Listing" ADD CONSTRAINT "Listing_performanceId_fkey" FOREIGN KEY ("performanceId") REFERENCES "PerformanceScale"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
@@ -211,28 +248,35 @@ ALTER TABLE "Listing" ADD CONSTRAINT "Listing_gameId_fkey" FOREIGN KEY ("gameId"
 ALTER TABLE "Listing" ADD CONSTRAINT "Listing_deviceId_fkey" FOREIGN KEY ("deviceId") REFERENCES "Device"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Vote" ADD CONSTRAINT "Vote_listingId_fkey" FOREIGN KEY ("listingId") REFERENCES "Listing"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Vote" ADD CONSTRAINT "Vote_listingId_fkey" FOREIGN KEY ("listingId") REFERENCES "Listing"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Vote" ADD CONSTRAINT "Vote_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Vote" ADD CONSTRAINT "Vote_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Comment" ADD CONSTRAINT "Comment_parentId_fkey" FOREIGN KEY ("parentId") REFERENCES "Comment"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Comment" ADD CONSTRAINT "Comment_listingId_fkey" FOREIGN KEY ("listingId") REFERENCES "Listing"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Comment" ADD CONSTRAINT "Comment_listingId_fkey" FOREIGN KEY ("listingId") REFERENCES "Listing"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Comment" ADD CONSTRAINT "Comment_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Comment" ADD CONSTRAINT "Comment_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "CommentVote" ADD CONSTRAINT "CommentVote_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "CommentVote" ADD CONSTRAINT "CommentVote_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "CommentVote" ADD CONSTRAINT "CommentVote_commentId_fkey" FOREIGN KEY ("commentId") REFERENCES "Comment"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "CommentVote" ADD CONSTRAINT "CommentVote_commentId_fkey" FOREIGN KEY ("commentId") REFERENCES "Comment"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "ListingApproval" ADD CONSTRAINT "ListingApproval_approvedById_fkey" FOREIGN KEY ("approvedById") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "ListingCustomFieldValue" ADD CONSTRAINT "ListingCustomFieldValue_listingId_fkey" FOREIGN KEY ("listingId") REFERENCES "Listing"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "ListingApproval" ADD CONSTRAINT "ListingApproval_listingId_fkey" FOREIGN KEY ("listingId") REFERENCES "Listing"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "ListingCustomFieldValue" ADD CONSTRAINT "ListingCustomFieldValue_customFieldDefinitionId_fkey" FOREIGN KEY ("customFieldDefinitionId") REFERENCES "CustomFieldDefinition"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_EmulatorToSystem" ADD CONSTRAINT "_EmulatorToSystem_A_fkey" FOREIGN KEY ("A") REFERENCES "Emulator"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_EmulatorToSystem" ADD CONSTRAINT "_EmulatorToSystem_B_fkey" FOREIGN KEY ("B") REFERENCES "System"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
