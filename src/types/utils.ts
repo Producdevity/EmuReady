db5d9c220393c94a30078184c6140edbe2132d36
Yassine Gherbi <y.gherbi.dev@gmail.com>
2025-06-01 10:58:07 +0200

    feat: add Prettify type for improved readability of complex types

--- src/types/utils.ts
+++ src/types/utils.ts
@@ -5,3 +5,13 @@
 export type Nullable<T> = T | null
 
 export type Maybe<T> = T | null | undefined
+
+/**
+ * Prettify complex types for better readability in IDEs
+ * https://twitter.com/mattpocockuk/status/1622730173446557697
+ * https://timdeschryver.dev/bits/pretty-typescript-types
+ * https://www.totaltypescript.com/concepts/the-prettify-helper
+ */
+export type Prettify<T> = {
+  [K in keyof T]: T[K]
+} & {}
