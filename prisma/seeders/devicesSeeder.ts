001f555fa794558d2a165f1e058d83939f4a2d6c
Yassine Gherbi <y.gherbi.dev@gmail.com>
2025-06-03 23:24:47 +0200

    feat: update device and SoC data with new models and specifications

--- prisma/seeders/devicesSeeder.ts
+++ prisma/seeders/devicesSeeder.ts
@@ -7,26 +7,35 @@ type DeviceData = {
 }
 
 const devices: DeviceData[] = [
+  // ASUS
   { brandName: 'ASUS', modelName: 'ROG Ally RC71L', socName: 'AMD Z1 Extreme' },
   { brandName: 'ASUS', modelName: 'ROG Ally RC71L-NH001W', socName: 'AMD Z1' },
   { brandName: 'ASUS', modelName: 'ROG Phone 5', socName: 'Snapdragon 888' },
   {
     brandName: 'ASUS',
     modelName: 'ROG Phone 6',
-    socName: 'Snapdragon 8 Gen 1',
+    socName: 'Snapdragon 8+ Gen 1',
   },
   {
     brandName: 'ASUS',
     modelName: 'ROG Phone 7',
     socName: 'Snapdragon 8 Gen 2',
   },
-  { brandName: 'AYANEO', modelName: 'Air Plus', socName: 'AMD Z1 Extreme' },
-  { brandName: 'AYANEO', modelName: 'Air Pro', socName: 'AMD Z1 Extreme' },
-  { brandName: 'AYANEO', modelName: 'Air', socName: 'AMD Z1' },
-  { brandName: 'AYANEO', modelName: 'Kun', socName: 'AMD Z1 Extreme' },
-  { brandName: 'AYANEO', modelName: 'Next Advance', socName: 'AMD Z1 Extreme' },
-  { brandName: 'AYANEO', modelName: 'Next Pro', socName: 'AMD Z1 Extreme' },
-  { brandName: 'AYANEO', modelName: 'Next', socName: 'AMD Z1' },
+
+  // AYANEO
+  { brandName: 'AYANEO', modelName: 'Air Plus', socName: 'AMD Ryzen 7 6800U' },
+  { brandName: 'AYANEO', modelName: 'Air Pro', socName: 'AMD Ryzen 7 5825U' },
+  { brandName: 'AYANEO', modelName: 'Air', socName: 'AMD Ryzen 5 5560U' },
+  { brandName: 'AYANEO', modelName: 'Kun', socName: 'AMD Ryzen 7 7840U' },
+  {
+    brandName: 'AYANEO',
+    modelName: 'Next Advance',
+    socName: 'AMD Ryzen 7 5800U',
+  },
+  { brandName: 'AYANEO', modelName: 'Next Pro', socName: 'AMD Ryzen 7 5825U' },
+  { brandName: 'AYANEO', modelName: 'Next', socName: 'AMD Ryzen 7 5825U' },
+
+  // AYN
   { brandName: 'AYN', modelName: 'Odin 2 Max', socName: 'Snapdragon 8 Gen 2' },
   { brandName: 'AYN', modelName: 'Odin 2 Mini', socName: 'Snapdragon 8 Gen 2' },
   {
@@ -35,13 +44,20 @@ const devices: DeviceData[] = [
     socName: 'Snapdragon 8 Gen 2',
   },
   { brandName: 'AYN', modelName: 'Odin 2', socName: 'Snapdragon 8 Gen 2' },
-  { brandName: 'AYN', modelName: 'Odin', socName: 'Snapdragon 865' },
+  { brandName: 'AYN', modelName: 'Odin', socName: 'Snapdragon 845' },
+
+  // Anbernic
   { brandName: 'Anbernic', modelName: 'RG505', socName: 'Tiger T618' },
-  { brandName: 'GPD', modelName: 'Win 4', socName: 'AMD Z1 Extreme' },
-  { brandName: 'GPD', modelName: 'Win Max 2', socName: 'AMD Z1 Extreme' },
+  { brandName: 'Anbernic', modelName: 'RG406H', socName: 'Tiger T820' },
+  { brandName: 'Anbernic', modelName: 'RG406V', socName: 'Tiger T820' },
+
+  // GPD
+  { brandName: 'GPD', modelName: 'Win 4', socName: 'AMD Ryzen 7 6800U' },
+  { brandName: 'GPD', modelName: 'Win Max 2', socName: 'AMD Ryzen 7 7840U' },
   { brandName: 'GPD', modelName: 'XP Plus', socName: 'Dimensity 1200' },
-  { brandName: 'GPD', modelName: 'XP Plus', socName: 'Dimensity 1200' },
-  { brandName: 'GPD', modelName: 'XP', socName: 'Dimensity 1200' },
+  { brandName: 'GPD', modelName: 'XP', socName: 'Helio G95' },
+
+  // Lenovo
   { brandName: 'Lenovo', modelName: 'Legion Go', socName: 'AMD Z1 Extreme' },
   {
     brandName: 'Lenovo',
@@ -51,50 +67,80 @@ const devices: DeviceData[] = [
   {
     brandName: 'Lenovo',
     modelName: 'Legion Phone Duel',
-    socName: 'Snapdragon 865',
+    socName: 'Snapdragon 865+',
   },
+
+  // Logitech
   { brandName: 'Logitech', modelName: 'G Cloud', socName: 'Snapdragon 720G' },
+
+  // MSI
   { brandName: 'MSI', modelName: 'Claw', socName: 'Intel Core Ultra 7 155H' },
+
+  // OnePlus
   { brandName: 'OnePlus', modelName: '10 Pro', socName: 'Snapdragon 8 Gen 1' },
-  { brandName: 'OnePlus', modelName: '10T', socName: 'Snapdragon 8 Gen 1' },
+  { brandName: 'OnePlus', modelName: '10T', socName: 'Snapdragon 8+ Gen 1' },
   { brandName: 'OnePlus', modelName: '11', socName: 'Snapdragon 8 Gen 2' },
-  { brandName: 'OnePlus', modelName: '11R', socName: 'Snapdragon 8 Gen 1' },
+  { brandName: 'OnePlus', modelName: '11R', socName: 'Snapdragon 8+ Gen 1' },
   { brandName: 'OnePlus', modelName: '12', socName: 'Snapdragon 8 Gen 3' },
   { brandName: 'OnePlus', modelName: '12R', socName: 'Snapdragon 8 Gen 2' },
-  { brandName: 'OnePlus', modelName: '13', socName: 'Snapdragon 8 Gen 3' },
-  { brandName: 'OnePlus', modelName: '13R', socName: 'Snapdragon 8 Gen 2' },
+  { brandName: 'OnePlus', modelName: '13', socName: 'Snapdragon 8 Elite' },
+  { brandName: 'OnePlus', modelName: '13R', socName: 'Snapdragon 8 Gen 3' },
   { brandName: 'OnePlus', modelName: '9 Pro', socName: 'Snapdragon 888' },
   { brandName: 'OnePlus', modelName: 'Open', socName: 'Snapdragon 8 Gen 2' },
+
+  // Pimax
   { brandName: 'Pimax', modelName: 'Portal', socName: 'Snapdragon 8 Gen 2' },
+
+  // Razer
   { brandName: 'Razer', modelName: 'Edge 5G', socName: 'Snapdragon 8 Gen 1' },
   { brandName: 'Razer', modelName: 'Edge', socName: 'Snapdragon 8 Gen 1' },
-  { brandName: 'RedMagic', modelName: '10 Air', socName: 'Snapdragon 8 Gen 3' },
-  { brandName: 'RedMagic', modelName: '10 Pro', socName: 'Snapdragon 8 Gen 3' },
+
+  // RedMagic
+  { brandName: 'RedMagic', modelName: '6R', socName: 'Snapdragon 888' },
   { brandName: 'RedMagic', modelName: '7', socName: 'Snapdragon 8 Gen 1' },
+  { brandName: 'RedMagic', modelName: '7 Pro', socName: 'Snapdragon 8 Gen 1' },
+  {
+    brandName: 'RedMagic',
+    modelName: '7S Pro',
+    socName: 'Snapdragon 8+ Gen 1',
+  },
   { brandName: 'RedMagic', modelName: '8 Pro', socName: 'Snapdragon 8 Gen 2' },
+  { brandName: 'RedMagic', modelName: '8S Pro', socName: 'Snapdragon 8 Gen 2' },
   { brandName: 'RedMagic', modelName: '9 Pro', socName: 'Snapdragon 8 Gen 3' },
+  { brandName: 'RedMagic', modelName: '9S Pro', socName: 'Snapdragon 8 Gen 3' },
+  { brandName: 'RedMagic', modelName: '10 Air', socName: 'Snapdragon 8 Gen 3' },
+  { brandName: 'RedMagic', modelName: '10 Pro', socName: 'Snapdragon 8 Elite' },
+
   { brandName: 'Retroid', modelName: 'Pocket 3', socName: 'Tiger T618' },
   {
     brandName: 'Retroid',
     modelName: 'Pocket 4 Pro',
-    socName: 'Dimensity 1200',
+    socName: 'Dimensity 1100',
   },
-  { brandName: 'Retroid', modelName: 'Pocket 4', socName: 'Tiger T820' },
-  { brandName: 'Retroid', modelName: 'Pocket 5', socName: 'Tiger T820' },
+  { brandName: 'Retroid', modelName: 'Pocket 4', socName: 'Dimensity 900' },
+  { brandName: 'Retroid', modelName: 'Pocket 5', socName: 'Snapdragon 865' },
   {
     brandName: 'Retroid',
     modelName: 'Pocket Classic',
     socName: 'Allwinner H700',
   },
-  { brandName: 'Retroid', modelName: 'Pocket Flip 2', socName: 'Tiger T618' },
+  {
+    brandName: 'Retroid',
+    modelName: 'Pocket Flip 2 - SD',
+    socName: 'Snapdragon 865',
+  },
   { brandName: 'Retroid', modelName: 'Pocket Flip', socName: 'Allwinner A133' },
   {
     brandName: 'Retroid',
     modelName: 'Pocket Mini V2',
-    socName: 'Allwinner A133',
+    socName: 'Snapdragon 865',
   },
-  { brandName: 'Retroid', modelName: 'Pocket Mini', socName: 'Allwinner H700' },
+  { brandName: 'Retroid', modelName: 'Pocket Mini', socName: 'Snapdragon 865' },
+
+  // Valve
   { brandName: 'Valve', modelName: 'Steam Deck', socName: 'AMD Custom APU' },
+
+  // Xiaomi
   {
     brandName: 'Xiaomi',
     modelName: 'Black Shark 4',
