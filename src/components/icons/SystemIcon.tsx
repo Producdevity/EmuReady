a11e69672c36ccc3946ede1bf3d1c9cfcd5d38af
Producdevity <y.gherbi.dev@gmail.com>
2025-06-14 01:26:31 +0200

    feat: add tooltip functionality to SystemIcon for improved user interaction

--- src/components/icons/SystemIcon.tsx
+++ src/components/icons/SystemIcon.tsx
@@ -1,3 +1,4 @@
+import { Tooltip, TooltipContent, TooltipTrigger } from '@/components/ui'
 import { cn } from '@/lib/utils'
 import { type Maybe } from '@/types/utils'
 import getSystemIcon from './systems/getSystemIcon'
@@ -22,16 +23,23 @@ function SystemIcon(props: Props) {
 
   const sizeClass = props.size ? sizeClasses[props.size] : sizeClasses.md
 
-  return (
-    <div
-      className={cn(
-        'flex items-center justify-center dark:bg-gray-200 dark:rounded-2xl dark:p-1 overflow-hidden',
-        sizeClass,
-        props.className,
-      )}
-    >
-      {IconComponent ? <IconComponent /> : props.name}
-    </div>
+  return IconComponent ? (
+    <Tooltip>
+      <TooltipTrigger asChild>
+        <div
+          className={cn(
+            'flex items-center justify-center dark:bg-gray-200 dark:rounded-2xl dark:p-1 overflow-hidden',
+            sizeClass,
+            props.className,
+          )}
+        >
+          <IconComponent />
+        </div>
+      </TooltipTrigger>
+      <TooltipContent side="top">{props.name}</TooltipContent>
+    </Tooltip>
+  ) : (
+    props.name
   )
 }
 
