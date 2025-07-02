a7eb2235e8736bc4e6b6f91dc1c244a0fbef8485
Producdevity <y.gherbi.dev@gmail.com>
2025-07-02 16:13:48 +0200

    chore: bump version to 0.7.40 in service worker and package.json

--- public/service-worker.js
+++ public/service-worker.js
@@ -39,7 +39,7 @@
 /* ------------------------------------------------------------------ */
 
 /** Name of the runtime cache used by this Service Worker. */
-const CACHE_NAME = 'emuready_v0.7.39'
+const CACHE_NAME = 'emuready_v0.7.40'
 
 /** URLs cached during the installation step. */
 const urlsToCache = [
