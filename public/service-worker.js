ec415eea7d2ca0d971f87a461e26d90a648feb1d
Producdevity <y.gherbi.dev@gmail.com>
2025-08-06 23:49:37 +0200

    chore: update version to 0.8.12 and cache name in service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -39,7 +39,7 @@
 /* ------------------------------------------------------------------ */
 
 /** Name of the runtime cache used by this Service Worker. */
-const CACHE_NAME = 'emuready_v0.8.11'
+const CACHE_NAME = 'emuready_v0.8.12'
 
 /** URLs cached during the installation step. */
 const urlsToCache = [
