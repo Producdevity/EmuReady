ebb4d859cbcf0eb4565605383a9dd4cd7ce374d6
Producdevity <y.gherbi.dev@gmail.com>
2025-08-07 14:32:18 +0200

    feat: bump version to 0.8.13 and update service worker cache name

--- public/service-worker.js
+++ public/service-worker.js
@@ -39,7 +39,7 @@
 /* ------------------------------------------------------------------ */
 
 /** Name of the runtime cache used by this Service Worker. */
-const CACHE_NAME = 'emuready_v0.8.12'
+const CACHE_NAME = 'emuready_v0.8.13'
 
 /** URLs cached during the installation step. */
 const urlsToCache = [
