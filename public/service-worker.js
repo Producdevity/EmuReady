347dae2715f3b6dab32729fe589c051c6d3e461a
Producdevity <y.gherbi.dev@gmail.com>
2025-08-25 13:47:15 +0200

    chore: update lint-staged configuration and increment version to 0.10.0

--- public/service-worker.js
+++ public/service-worker.js
@@ -39,7 +39,7 @@
 /* ------------------------------------------------------------------ */
 
 /** Name of the runtime cache used by this Service Worker. */
-const CACHE_NAME = 'emuready_v0.9.9'
+const CACHE_NAME = 'emuready_v0.10.0'
 
 /** URLs cached during the installation step */
 const urlsToCache = [
