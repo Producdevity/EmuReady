ae1b5a4f43f6d801562abf0eef0204b1f0787a0c
ObfuscatedVoid <ObfuscatedVoid@proton.me>
2025-09-16 23:16:08 +0200

    chore(deps): update package versions to 0.10.13 and Prisma to 6.16.2

--- public/service-worker.js
+++ public/service-worker.js
@@ -39,7 +39,7 @@
 /* ------------------------------------------------------------------ */
 
 /** Name of the runtime cache used by this Service Worker. */
-const CACHE_NAME = 'emuready_v0.10.12'
+const CACHE_NAME = 'emuready_v0.10.13'
 
 /** URLs cached during the installation step */
 const urlsToCache = [
--- public/sw-register.js
+++ public/sw-register.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'emuready_v0.10.12'
+const CACHE_NAME = 'emuready_v0.10.13'
 
 // Service worker registration script
 if ('serviceWorker' in navigator) {
