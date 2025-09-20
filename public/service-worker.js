fb160a473e2f4edee241d5128466da140f57c792
ObfuscatedVoid <ObfuscatedVoid@proton.me>
2025-09-20 13:12:35 +0200

    chore: update version to 0.10.18 and adjust cache name in service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -39,7 +39,7 @@
 /* ------------------------------------------------------------------ */
 
 /** Name of the runtime cache used by this Service Worker. */
-const CACHE_NAME = 'emuready_v0.10.17'
+const CACHE_NAME = 'emuready_v0.10.18'
 
 /** URLs cached during the installation step */
 const urlsToCache = [
--- public/sw-register.js
+++ public/sw-register.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'emuready_v0.10.17'
+const CACHE_NAME = 'emuready_v0.10.18'
 
 // Service worker registration script
 if ('serviceWorker' in navigator) {
