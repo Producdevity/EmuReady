effd5efde25368e6e7c8a548f5c5b693a23f8852
ObfuscatedVoid <ObfuscatedVoid@proton.me>
2025-09-24 23:24:03 +0200

    chore: bump version to 0.10.26 in package.json and package-lock.json

--- public/service-worker.js
+++ public/service-worker.js
@@ -39,7 +39,7 @@
 /* ------------------------------------------------------------------ */
 
 /** Name of the runtime cache used by this Service Worker. */
-const CACHE_NAME = 'emuready_v0.10.25'
+const CACHE_NAME = 'emuready_v0.10.26'
 
 /** URLs cached during the installation step */
 const urlsToCache = [
